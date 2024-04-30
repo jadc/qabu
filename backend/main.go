76c238356588f523bd85c929f055d9199f4436f9
jadc <chehimj@gmail.com>
2024-04-29 19:14:48 -0600

    feat: dockerized postgresql and go backend

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
+    "context"
     "fmt"
     "os"
     //    "net/http"
     //    "database/sql"
+    "github.com/jackc/pgx/v5/pgxpool"
 )
 
 func main() {
-    database_url := os.Getenv("DATABASE_URL")
+    // Retrieve POSTGRES_URL from the environment
+    database_url := os.Getenv("POSTGRES_URL")
     if database_url == "" {
-        fmt.Println("Environmental variable DATABASE_URL is not set")
+        fmt.Println("Environmental variable POSTGRES_URL is not set")
         os.Exit(1)
     }
 
-    fmt.Println(database_url)
+    // Connect to PostgreSQL
+    db, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+    // Query the database
+	var greeting string
+	err = db.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(greeting)
 }
