7fd0216ed0afd0d59b2863db4d893b307d166b75
jadc <chehimj@gmail.com>
2024-04-29 19:16:57 -0600

    fix: formatting

--- backend/main.go
+++ backend/main.go
@@ -19,19 +19,19 @@ func main() {
 
     // Connect to PostgreSQL
     db, err := pgxpool.New(context.Background(), os.Getenv("POSTGRES_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
-		os.Exit(1)
-	}
-	defer db.Close()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+        os.Exit(1)
+    }
+    defer db.Close()
 
     // Query the database
-	var greeting string
-	err = db.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-		os.Exit(1)
-	}
+    var greeting string
+    err = db.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+        os.Exit(1)
+    }
 
-	fmt.Println(greeting)
+    fmt.Println(greeting)
 }
